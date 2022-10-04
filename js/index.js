cf82a9993db38cdfd6b8ccbd0e24188cc7f4e30a
Zzzoga <105968987+Zzzoga@users.noreply.github.com>
2022-10-04 12:30:58 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,17 @@
+// Стилизация инпута с выбором файла
+let inputs = document.querySelectorAll('.sending__input.file');
+Array.prototype.forEach.call(inputs, function (input) {
+  let label = input.nextElementSibling,
+    labelVal = label.querySelector('.input__file-button-text').innerText;
+
+  input.addEventListener('change', function (e) {
+    let countFiles = '';
+    if (this.files && this.files.length >= 1)
+      countFiles = this.files.length;
+
+    if (countFiles)
+      label.querySelector('.input__file-button-text').innerText = 'Выбрано файлов: ' + countFiles;
+    else
+      label.querySelector('.input__file-button-text').innerText = labelVal;
+  });
+});
